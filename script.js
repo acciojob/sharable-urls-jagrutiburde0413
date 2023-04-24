1888468269e87d26e3957caf91ec5c7f158ef822
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-04-25 00:44:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // your code here
+  const form = document.querySelector('form');
+      const nameInput = document.querySelector('#name');
+      const yearInput = document.querySelector('#year');
+      const urlText = document.querySelector('#url');
+      const button = document.querySelector('#button');
+
+      button.addEventListener('click', () => {
+        const name = encodeURIComponent(nameInput.value);
+        const year = encodeURIComponent(yearInput.value);
+        const url = `https://localhost:8080/?name=${name}&year=${year}`;
+        urlText.textContent = url;
+      });
